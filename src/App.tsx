a3157058f5844075f5662f99fc672ec3d7856a17
pawel.basiak <pawel.basiak@kogniasports.com>
2022-12-31 11:02:47 +0100

    feat: base app config

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  Link,
+  Outlet,
+  RouterProvider,
+  createReactRouter,
+  createRouteConfig
+} from "@tanstack/react-router";
 
-function App() {
+const rootRoute = createRouteConfig({
+    "component": () => <>
+      <div>
+        <Link to="/">Home</Link> <Link to="/about">About</Link>
+      </div>
+      <hr />
+      <Outlet />
+    </>
+
+  }),
+
+  indexRoute = rootRoute.createRoute({
+    "path": "/",
+    "component": Index
+  }),
+
+  aboutRoute = rootRoute.createRoute({
+    "path": "/about",
+    "component": About
+  }),
+
+  routeConfig = rootRoute.addChildren([
+    indexRoute,
+    aboutRoute
+  ]),
+
+  router = createReactRouter({routeConfig});
+
+export function App () {
+  return <RouterProvider router={router} />;
+}
+
+function Index () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Welcome Home!</h3>
     </div>
   );
 }
 
-export default App;
+function About () {
+  return <div>Hello from About!</div>;
+}
+
+
+declare module "@tanstack/react-router" {
+  interface RegisterRouter {
+    router: typeof router
+  }
+}
---
+++ src/theme/theme.tsx
@@ -0,0 +1,9 @@
+import {createTheme} from "@mui/material";
+
+export const AppTheme = createTheme({
+    "palette": {
+        "primary": {
+            "main": "#2962ff"
+        }
+    }
+});
