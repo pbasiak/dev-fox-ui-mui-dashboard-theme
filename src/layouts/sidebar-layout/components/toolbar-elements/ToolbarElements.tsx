31ab3037d2dbfe744407b820ab6324ea82b75f65
pawel.basiak <pawel.basiak@kogniasports.com>
2023-04-15 15:34:47 +0200

    feat: add search input in toolbar

--- src/layouts/sidebar-layout/components/toolbar-elements/ToolbarElements.tsx
+++ src/layouts/sidebar-layout/components/toolbar-elements/ToolbarElements.tsx
@@ -1,14 +1,23 @@
 import { UserMenu } from '../user-menu/UserMenu';
-import { Box } from '@mui/material';
+import { Stack, TextField } from '@mui/material';
 import { useCurrentUser } from '../../../../hooks/api/use-current-user/useCurrentUser';
+import { Search } from '@mui/icons-material';
 
 export const ToolbarElements = () => {
   const { data: user } = useCurrentUser();
 
   if (!user) return null;
   return (
-    <Box>
+    <Stack direction={'row'} spacing={2}>
+      <TextField
+        variant={'outlined'}
+        size={'small'}
+        placeholder={'Search...'}
+        InputProps={{
+          endAdornment: <Search sx={{ color: 'grey.500' }} />,
+        }}
+      />
       <UserMenu user={user} />
-    </Box>
+    </Stack>
   );
 }
