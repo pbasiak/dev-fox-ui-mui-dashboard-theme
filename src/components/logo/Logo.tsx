cb6224292a9c907789d4af0ba9749ff41556d81e
pbasiak <pawel.basiak01@gmail.com>
2024-04-03 21:22:52 +0200

    fix: adjust pre login screens

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -4,13 +4,19 @@ import { useNavigate } from 'react-router-dom';
 import { routes } from '../../contants/routes';
 import logo from '../../assets/logo.png';
 
-export const Logo = () => {
+interface Params {
+  invertImage?: boolean;
+}
+
+export const Logo = ({ invertImage }: Params) => {
   const navigate = useNavigate();
   const theme = useTheme();
 
   return (
     <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '100%', cursor: 'pointer', '&:hover': { opacity: 0.8 } }} onClick={() => navigate(routes.dashboard)} component={'a'}>
-      <Box component={'img'} src={logo} alt={''} sx={{ maxWidth: '40px', mr: 2, filter: `invert(${theme.palette.mode === 'light' ? '0' : '1'})` }}  />
+      <Box sx={{ filter: `invert(${invertImage ? '1' : '0'})` }}>
+        <Box component={'img'} src={logo} alt={''} sx={{ maxWidth: '40px', mr: 2, filter: `invert(${theme.palette.mode === 'light' ? '0' : '1'})` }}  />
+      </Box>
       <Typography variant={'h4'} component={'h1'} fontSize={'1.5rem'} fontWeight={'fontWeightBold'}>DevFoxUI</Typography>
     </Box>
   )
