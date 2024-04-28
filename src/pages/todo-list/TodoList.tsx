32ad43453ffd507d3e169e400fb5b908c4c973bd
pbasiak <pawel.basiak01@gmail.com>
2024-04-28 09:35:41 +0200

    feat: update task app with hide completed and ui adjustments

--- src/pages/todo-list/TodoList.tsx
+++ src/pages/todo-list/TodoList.tsx
@@ -1,9 +1,11 @@
 import React, { useCallback, useState } from 'react';
 import { v4 as uuid } from 'uuid';
-import { Typography, TextField, Button, List, Stack, Container } from '@mui/material';
+import { Typography, Button, List, Container, Paper, Stack } from '@mui/material';
 import { PageHeader } from '../../components/page-header/PageHeader';
 import { Task } from './types/task';
 import { TodoListItem } from './components/todo-list-item/TodoListItem';
+import { Add } from '@mui/icons-material';
+import { AddTodo } from './components/add-todo/AddTodo.tsx';
 
 const defaultTasks: Task[] = [
   { id: uuid(), name: 'Task 1', completed: true },
@@ -12,15 +14,25 @@ const defaultTasks: Task[] = [
 ];
 
 export const TodoList: React.FC = () => {
-  const [task, setTask] = useState<string>('');
   const [tasks, setTasks] = useState<Task[]>(defaultTasks);
+  const [addTaskModalOpen, setAddTaskModalOpen] = useState<boolean>(false);
+  const [hideCompleted, setHideCompleted] = useState<boolean>(false);
 
-  const handleAddTask = () => {
-    if (task.trim() !== '') {
-      setTasks([...tasks, { id: uuid(), name: task, completed: false }]);
-      setTask('');
+  const handleEditTask = useCallback(
+    (id: string, name: string) => {
+      setTasks(tasks.map((task) => (task.id === id ? { ...task, name } : task)));
+    },
+    [tasks],
+  );
+
+  const handleAddTask = (value: string) => {
+    if (value.trim() !== '') {
+      setTasks([...tasks, { id: uuid(), name: value, completed: false }]);
     }
   };
+
+  const handleAddTaskModalOpen = useCallback(() => setAddTaskModalOpen(true), []);
+  const handleAddTaskModalClose = useCallback(() => setAddTaskModalOpen(false), []);
 
   const handleDeleteTask = useCallback(
     (id: string) => {
@@ -35,30 +47,48 @@ export const TodoList: React.FC = () => {
 
   return (
     <Container>
-      <PageHeader title={'Todo List'} breadcrumbs={['Todo List']} />
-      <Stack direction={'row'} spacing={2} justifyContent={'space-between'} marginBottom={2}>
-        <TextField
-          label='Add a task'
-          variant='outlined'
-          value={task}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setTask(e.target.value)}
-          sx={{ flexGrow: 1 }}
-        />
-        <Button variant='contained' color='primary' onClick={handleAddTask}>
-          Add Task
-        </Button>
-      </Stack>
-      {tasks.length === 0 ? (
-        <Typography variant='body1' textAlign={'center'} marginTop={4} fontWeight={'fontWeightMedium'}>
-          No tasks added yet.
-        </Typography>
-      ) : (
-        <List>
-          {tasks.map((task) => (
-            <TodoListItem key={task.id} task={task} onDelete={handleDeleteTask} onComplete={handleCompleteTask} />
-          ))}
-        </List>
-      )}
+      <PageHeader
+        title={'Tasks'}
+        breadcrumbs={['Tasks']}
+        renderRight={
+          <Button variant='contained' color='primary' onClick={handleAddTaskModalOpen} startIcon={<Add />}>
+            Add Task
+          </Button>
+        }
+      />
+
+      <Paper sx={{ px: 2, py: 3 }}>
+        <Stack>
+          <Button variant='outlined' onClick={() => setHideCompleted(!hideCompleted)}>
+            {hideCompleted ? 'Show completed' : 'Hide completed'}
+          </Button>
+        </Stack>
+        {tasks.length === 0 ? (
+          <Typography variant='body1' textAlign={'center'} marginTop={4} fontWeight={'fontWeightMedium'}>
+            No tasks added yet.
+          </Typography>
+        ) : (
+          <List>
+            {tasks.map((task) => {
+              if (hideCompleted && task.completed) {
+                return null;
+              }
+
+              return (
+                <TodoListItem
+                  key={task.id}
+                  task={task}
+                  onDelete={handleDeleteTask}
+                  onComplete={handleCompleteTask}
+                  onEdit={handleEditTask}
+                />
+              );
+            })}
+          </List>
+        )}
+      </Paper>
+
+      <AddTodo open={addTaskModalOpen} handleClose={handleAddTaskModalClose} onSubmit={handleAddTask} />
     </Container>
   );
 };
