ee720d202661e7e8e0b6751c488567fbb7174e10
pbasiak <pawel.basiak01@gmail.com>
2024-05-05 19:07:19 +0200

    fix: replace url images with local assets (#65)

--- src/hooks/api/use-blog-posts/index.ts
+++ src/hooks/api/use-blog-posts/index.ts
@@ -2,6 +2,19 @@ import { useQuery } from '@tanstack/react-query';
 import posts from '../../../mocks/blog/blog-posts.json';
 import { BlogPost } from '../../../pages/blog/blog-page/types/blogPost.ts';
 
+import post1 from '../../../mocks/blog/assets/post1.png';
+import post2 from '../../../mocks/blog/assets/post2.png';
+import post3 from '../../../mocks/blog/assets/post3.png';
+import post4 from '../../../mocks/blog/assets/post4.png';
+import post5 from '../../../mocks/blog/assets/post5.png';
+import post6 from '../../../mocks/blog/assets/post6.png';
+
+const images = [post1, post2, post3, post4, post5, post6];
+
 export const useBlogPosts = () => {
-  return useQuery<BlogPost[]>({ queryKey: ['blog-posts'], queryFn: () => posts.posts });
+  const mappedPosts = posts.posts.map((post, index) => ({ ...post, image: images[index] ?? '' }));
+  return useQuery<BlogPost[]>({
+    queryKey: ['blog-posts'],
+    queryFn: () => mappedPosts,
+  });
 };
--- src/hooks/api/use-current-user/useCurrentUser.ts
+++ src/hooks/api/use-current-user/useCurrentUser.ts
@@ -1,7 +1,16 @@
 import { useQuery } from '@tanstack/react-query';
-import users from '../../../mocks/users.json';
+import users from '../../../mocks/users/users.json';
 import { CurrentUserReturn } from '../use-user/types';
+import mockAvatar from '../../../mocks/users/assets/avatar.png';
+import mockCover from '../../../mocks/users/assets/cover.png';
 
 export const useCurrentUser = (): CurrentUserReturn => {
-  return useQuery({ queryKey: ['currentUser'], queryFn: () => users.users[0] });
+  return useQuery({
+    queryKey: ['currentUser'],
+    queryFn: () => ({
+      ...users.users[0],
+      image: users.users[0].image || mockAvatar,
+      profileBackground: mockCover,
+    }),
+  });
 };
