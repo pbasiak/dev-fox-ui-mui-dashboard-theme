6351488fd62b1a09ac858c60adf2db0443e9dfb0
pbasiak <pawel.basiak01@gmail.com>
2024-06-22 14:00:34 +0200

    refactor: rename use navigation hook

--- src/layouts/sidebar-layout/components/navigation/components/navigation-item/navigation-item-nested/NavigationItemNested.tsx
+++ src/layouts/sidebar-layout/components/navigation/components/navigation-item/navigation-item-nested/NavigationItemNested.tsx
@@ -8,7 +8,7 @@ import List from '@mui/material/List';
 import { NavigationItemSimple } from '../navigation-item-simple/NavigationItemSimple';
 import { NavigationListItemButton, NavigationListItemIcon } from '../styled';
 import { useCallback } from 'react';
-import { useNavigation } from '../../../hooks/use-navigation/useNavigation';
+import { useAppNavigation } from '../../../hooks/use-app-navigation/useAppNavigation';
 import { Stack, Typography } from '@mui/material';
 
 interface Props {
@@ -16,7 +16,7 @@ interface Props {
 }
 
 export const NavigationItemNested = ({ item }: Props) => {
-  const { getIsOpen, toggleNavigationId } = useNavigation();
+  const { getIsOpen, toggleNavigationId } = useAppNavigation();
   const isOpen = getIsOpen(item.label);
 
   const handleToggleOpen = useCallback(() => {
--- src/layouts/sidebar-layout/components/navigation/hooks/use-app-navigation/useAppNavigation.ts
+++ src/layouts/sidebar-layout/components/navigation/hooks/use-app-navigation/useAppNavigation.ts
@@ -2,7 +2,7 @@ import { useAppDispatch, useAppSelector } from '../../../../../../store/hooks';
 import { toggleNavigationIdAction, toggleSidebarAction } from '../../../../../../store/sidebar/navigationSlice';
 import { useCallback } from 'react';
 
-export const useNavigation = () => {
+export const useAppNavigation = () => {
   const navigation = useAppSelector((state) => state.navigation.openNavigationIds);
   const isSidebarOpen = useAppSelector((state) => state.navigation.isSidebarOpen);
   const dispatch = useAppDispatch();
