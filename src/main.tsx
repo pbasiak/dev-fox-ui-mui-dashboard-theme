0710bda93bc3940e6d8d18ab53c6f7f38e37a161
pbasiak <pawel.basiak01@gmail.com>
2024-09-14 15:18:30 +0200

    feat: remove redux, add jotai (#85)

--- src/main.tsx
+++ src/main.tsx
@@ -6,14 +6,10 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { Provider } from 'react-redux';
-import { store } from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
       <App />
-    </Provider>
   </React.StrictMode>,
 );
