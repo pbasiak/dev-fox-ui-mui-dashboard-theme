1c0eb68e1ba4865e646e87d4c51b4bde9fef07ee
pbasiak <pawel.basiak01@gmail.com>
2024-09-14 22:35:54 +0200

    fix: update chart props (#88)
    * feat: replace chart js with nivo
    
    * feat: update chart

--- src/pages/dashboard/components/sales-widget/SalesWidget.tsx
+++ src/pages/dashboard/components/sales-widget/SalesWidget.tsx
@@ -30,60 +30,6 @@ export const SalesWidget = () => {
         graticuleLineColor='#dddddd'
         borderWidth={0.5}
         borderColor='#152538'
-        defs={[
-          {
-            id: 'dots',
-            type: 'patternDots',
-            background: 'inherit',
-            color: '#38bcb2',
-            size: 4,
-            padding: 1,
-            stagger: true,
-          },
-          {
-            id: 'lines',
-            type: 'patternLines',
-            background: 'inherit',
-            color: '#eed312',
-            rotation: -45,
-            lineWidth: 6,
-            spacing: 10,
-          },
-          {
-            id: 'gradient',
-            type: 'linearGradient',
-            colors: [
-              {
-                offset: 0,
-                color: '#000',
-              },
-              {
-                offset: 100,
-                color: 'inherit',
-              },
-            ],
-          },
-        ]}
-        fill={[
-          {
-            match: {
-              id: 'CAN',
-            },
-            id: 'dots',
-          },
-          {
-            match: {
-              id: 'CHN',
-            },
-            id: 'lines',
-          },
-          {
-            match: {
-              id: 'ATA',
-            },
-            id: 'gradient',
-          },
-        ]}
         legends={[
           {
             anchor: 'bottom-left',
